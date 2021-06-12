0a7d500f3caed11613b0f904d91e8c47ebd4f34c
Zuvo007 <duttasubhabrata123@gmail.com>
2021-06-13 03:08:35 +0530

    Grretings component added

---
+++ src/components/Greetings.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Greetings.css";
+import greetImg from "../asset/Group 114.png";
+
+function Greetings() {
+  return (
+    <div className="greetings-note-wrapper">
+      <div className="greeting-img">
+        <img src={greetImg} alt="" />
+      </div>
+      <div className="greeting-title">
+        <p>Welcome To Nature</p>
+      </div>
+      <div className="greeting-description">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
+          doloribus, quae ab voluptas obcaecati magnam numquam sit natus
+          blanditiis praesentium
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Greetings;
