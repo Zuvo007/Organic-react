637bd6c6baf869a9c61677dea6336c7d2b3d7553
Zuvo007 <duttasubhabrata123@gmail.com>
2021-06-13 14:31:57 +0530

    subscribe component updated

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Subscribe.css";
 import greetImg from "../asset/Group 114.png";
 
-function Subscribe() {
+const Subscribe = () => {
   return (
     <div className="subscribe-note-wrapper">
       <div className="subscribe-img">
@@ -18,16 +18,20 @@ function Subscribe() {
           blanditiis praesentium
         </p>
       </div>
-        <div className="subscribe-button-wrapper">
-            <div className="email">
-                <input type="email" placeholder="enter your email" className="email-id" />
-            </div>
-            <div className="btn">
-                <button className="subscribe-btn">Subscribe</button>
-            </div>
+      <div className="subscribe-button-wrapper">
+        <div className="email">
+          <input
+            type="email"
+            placeholder="enter your email"
+            className="email-id"
+          />
         </div>
+        <div className="btn">
+          <button className="subscribe-btn">Subscribe</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Subscribe;
