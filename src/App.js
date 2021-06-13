d429a468e670d3e0f98401c8e686b8a8290bb8d2
Zuvo007 <duttasubhabrata123@gmail.com>
2021-06-13 05:42:35 +0530

    Brief,Greeting component updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Brief from "./components/Brief";
+import Greetings from "./components/Greetings";
+import Imagecard from "./components/Imagecard";
+import Presentative from "./components/Presentative";
+import Subscribe from "./components/Subscribe";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Brief />
+      <Greetings />
+      <Imagecard />
+      <Presentative />
+      <Subscribe />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Navbar.css";
-import logo from '../asset/Group 220.png';
+import logo from "../asset/Group 220.png";
 
 const Navbar = () => {
   return (
@@ -12,25 +12,35 @@ const Navbar = () => {
         <nav>
           <ul className="nav-links-wrapper">
             <li className="nav-links active">
-              <a href="#" className="nav-link nav-link-1">Home</a>
+              <a href="#" className="nav-link nav-link-1">
+                Home
+              </a>
             </li>
             <li className="nav-links">
-              <a href="#" className="nav-link nav-link-2">Products</a>
+              <a href="#" className="nav-link nav-link-2">
+                Products
+              </a>
             </li>
             <li className="nav-links">
-              <a href="#" className="nav-link nav-link-3">Blogs</a>
+              <a href="#" className="nav-link nav-link-3">
+                Blogs
+              </a>
             </li>
             <li className="nav-links">
-              <a href="#" className="nav-link nav-link-4">About Us</a>
+              <a href="#" className="nav-link nav-link-4">
+                About Us
+              </a>
+            </li>
+            <li className="nav-links">
+              <a href="#" className="contact-link">
+                <button>Contact</button>
+              </a>
             </li>
           </ul>
         </nav>
-        <a href="#" className="contact-link">
-          <button>Contact</button>
-        </a>
       </header>
     </React.Fragment>
   );
-}
+};
 
 export default Navbar;
