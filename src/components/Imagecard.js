215fb6e4b1f3116fa9ea048ca2f424004cd6da59
Zuvo007 <duttasubhabrata123@gmail.com>
2021-06-13 14:28:42 +0530

    imagecard component updated

--- src/components/Imagecard.js
+++ src/components/Imagecard.js
@@ -1,23 +1,24 @@
-import React  from 'react';
+import React from "react";
 import "./Imagecard.css";
 import imgActive from "../asset/Group 205.png";
 import imgNormal from "../asset/Group 207.png";
 
-function Imagecard() {
-    const imageId = [1,2,3,4];
-    return (
-        <div className="image-card-wrapper">
-           {imageId.map(id => 
-           (<a href="#">
-           <img 
-            src = {id === 2 ? imgActive: imgNormal} 
-            key={id} 
-            alt="No Image Found" 
-            className={` image-card img-${id}`}
-            />
-            </a>))}
-        </div>
-    )
-}
+const Imagecard = () => {
+  const imagesId = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
+  return (
+    <div className="image-card-wrapper">
+      {imagesId.map((image) => (
+        <a href="#">
+          <img
+            src={image.id === 2 ? imgActive : imgNormal}
+            key={image.id}
+            alt="Not Found"
+            className={` image-card img-${image.id}`}
+          />
+        </a>
+      ))}
+    </div>
+  );
+};
 
-export default Imagecard
+export default Imagecard;
